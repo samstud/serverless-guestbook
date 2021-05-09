e683670668d5da134c4b4b15c177541cfdcfe9a3
samstud <83789540+samstud@users.noreply.github.com>
2021-05-09 14:28:48 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2fac0e7c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
